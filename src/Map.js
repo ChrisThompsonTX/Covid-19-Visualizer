3212c0d33e56e02136b18b801d073503646f31ac
Chris Thompson <chris.tx86@gmail.com>
2020-09-03 15:39:28 -0700

    setup circles for map

--- src/Map.js
+++ src/Map.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./map.css"
 import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 
-function Map({ center, zoom }) {
+function Map({ countries, center, zoom }) {
     return (
         <div className="map">
             <LeafletMap
@@ -13,6 +13,7 @@ function Map({ center, zoom }) {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
+                
             </LeafletMap>
         </div>
     )
