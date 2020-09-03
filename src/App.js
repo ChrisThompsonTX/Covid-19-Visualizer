ed89b042251ee7e2f0d265853015efd891268820
Chris Thompson <chris.tx86@gmail.com>
2020-09-03 15:19:48 -0700

    add map

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Map from './Map';
 import Table from './Table';
 import LineGraph from './LineGraph';
 import { sortData } from './util';
+import "leaflet/dist/leaflet.css";
 
 function App() {
   const [ countries, setCountries] = useState([]);
   const [ country, setCountry] = useState(['worldwide']);
   const [ countryInfo, setCountryInfo] = useState({});
   const [ tableData, setTableData] = useState([]);
+  const [ mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [ mapZoom, setMapZoom] = useState(3);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -83,7 +86,10 @@ function App() {
           <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
         </div>
 
-        <Map></Map>
+        <Map 
+          center={mapCenter}
+          zoom={mapZoom}
+        />
 
       </div>
       <Card className="app__right">
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import "./map.css"
+import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 
-function Map() {
+function Map({ center, zoom }) {
     return (
         <div className="map">
-            I'm the map.
+            <LeafletMap
+                center={center}
+                zoom={zoom}
+            >
+                <TileLayer 
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </LeafletMap>
         </div>
     )
 }
